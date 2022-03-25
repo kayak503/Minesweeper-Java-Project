9f8f70fcbfe2f0a3781051deaede7d28b2d40036
Nathan2846 <nathanabdelmalek@gmail.com>
2022-03-25 10:18:16 -0400

    Added javadocs

--- minesweeper/model/Location.java
+++ minesweeper/model/Location.java
@@ -4,24 +4,43 @@ public class Location {
     private int row;
     private int col;
 
+
+    /**
+     * A simple constructor that creates a Location object with given row and column
+     */
     public Location(int row, int col){
         this.row = row;
         this.col = col;
     }
 
+    /**
+     * 
+     * @return the row of the given location object
+     */
     public int getCol() {
         return col;
     }
 
+    /**
+     * 
+     * @return the column of the given location object
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * @return a string in the form (row, col)
+     */
     @Override
     public String toString(){
         return "( " + this.row + ", " + this.col + ")";
     }
 
+    /**
+     * A method to determine if this location is equal to the given object
+     * First ensures that the object is a location, then compares row and col
+     */
     @Override
     public boolean equals(Object other){
         if(other instanceof Location){
@@ -31,6 +50,10 @@ public class Location {
         return false;
     }
 
+    /**
+     * Simple hashcode. Converts row and column to strings and hashes them.
+     * @return the multiplication of the 2 hashcodes 
+     */
     @Override
     public int hashCode(){
         int rowHash = Integer.toString(this.row).hashCode();
--- minesweeper/model/Minesweeper.java
+++ minesweeper/model/Minesweeper.java
@@ -16,7 +16,16 @@ public class Minesweeper{
     private HashSet<Location> mines;
 
 
+    /**
+     * A constructor that sets up all fields to default values. Fills the set of mines
+     * with random mines. Fills the validSpots set with all remaining positions, as they are 
+     * all clickable by default
+     * @param rows The amount of rows in this board
+     * @param cols The amount of columns in this board
+     * @param mineCount The desired amount of mines in this board
+     */
     public Minesweeper(int rows, int cols, int mineCount){
+        //Initalise all variables
         this.rows = rows;
         this.cols = cols;
         this.mineCount = mineCount;
@@ -26,12 +35,14 @@ public class Minesweeper{
         this.validSpots = new HashSet<>();
         Random rng = new Random();
 
-        while(this.mines.size() != mineCount){
+        //Until the set reaches the desired size, add unique locations for mines
+        while(this.mines.size() != this.mineCount){
             int newRow = rng.nextInt(rows);
             int newCol = rng.nextInt(cols);
             this.mines.add(new Location(newRow, newCol));
         }
 
+        //Once all mines have been decided, make every other position valid
         for(int row = 0; row < rows; row++){
             for(int col = 0; col < cols; col++){
                 Location l = new Location(row, col);
@@ -42,51 +53,85 @@ public class Minesweeper{
         }
     }
 
+    /**
+     * 
+     * @return the amount of moves so far in the game
+     */
     public int getMovesCount(){return this.movesCount;}
+
+    /**
+     * 
+     * @return the current state of the game (in progress, not started, won or lost)
+     */
     public GameState getGameState(){return this.currentState;}
 
-    // returns the number of mines around the current location
+    
+    /**
+     * A helper function. Given a valid location, it will determine the quantity of
+     * mines in the 8 surrounding cells
+     * @param location The location we want to check at
+     * @return The number of mines that are found
+     */
     public int getMineCount(Location location){
         int count = 0;
         int row = location.getRow();
         int col = location.getCol();
+        //Bottom left
         if(mines.contains(new Location(row - 1, col - 1))){
             count++;
         }
+        //Left
         if(mines.contains(new Location(row, col - 1))){
             count++;
         }
+        //Upper left
         if(mines.contains(new Location(row + 1, col - 1))){
             count++;
         }
+        //Upper
         if(mines.contains(new Location(row + 1, col))){
             count++;
         }
+        //Upper right
         if(mines.contains(new Location(row + 1, col + 1))){
             count++;
         }
+        //Right
         if(mines.contains(new Location(row, col + 1))){
             count++;
         }
+        //Bottom right
         if(mines.contains(new Location(row - 1, col + 1))){
             count++;
         }
+        //Bottom
         if(mines.contains(new Location(row - 1, col))){
             count++;
         }
         return count;
     }
 
-    // updates the game state method
+    /**
+     * The game logic. Determines if a location is a valid move,
+     * bomb, or out of bounds.
+     * Bomb: sets game state to lost
+     * Out of bounds: throws a minesweeper exception
+     * valid: removes the move from set of valid moves. If the set is empty, set state to won
+     * @param location The location at which we want to make a move
+     * @throws Exception Called if the move is out of bounds
+     */
     public void makeSelection(Location location) throws Exception{ // should be minesweeper exception !!!
         if(location.getRow() > rows || location.getCol() > cols){
+            //Its not a calid move, throw the exception
             throw new MinesweeperException("Location out of Bounds");
         } else {
             if(this.mines.contains(location)){
+                //BOOM. End the game
                 this.currentState = GameState.LOST;
                 movesCount ++;
             }
             if(this.validSpots.contains(location)){
+                //Its valid, take it out of the set, increment moves, determine if game over
                 validSpots.remove(location);
                 movesCount ++;
                 this.currentState = GameState.IN_PROGRESS;
