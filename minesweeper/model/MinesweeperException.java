a85f6dd5fdba7e2d6e4245213fda81d708559c5b
Ari Becker <aribecker503@gmail.com>
2022-03-25 08:52:33 -0400

    fixed MinesweeperException class

--- minesweeper/model/MinesweeperException.java
+++ minesweeper/model/MinesweeperException.java
@@ -1,7 +1,7 @@
 package minesweeper.model;
 
 public class MinesweeperException extends java.lang.Exception{
-    public void MinesweeperException(String message) throws Exception{
+    public MinesweeperException(String message) throws Exception{
         throw new java.lang.Exception(message);
     }
     
