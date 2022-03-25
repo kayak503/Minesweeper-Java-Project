8d2d8fb10ec6ad9a75c67a9753d7f6aa29ff713e
Ari Becker <aribecker503@gmail.com>
2022-03-25 08:49:48 -0400

    added MinesweeperException class

---
+++ minesweeper/model/MinesweeperException.java
@@ -0,0 +1,8 @@
+package minesweeper.model;
+
+public class MinesweeperException extends java.lang.Exception{
+    public void MinesweeperException(String message) throws Exception{
+        throw new java.lang.Exception(message);
+    }
+    
+}
