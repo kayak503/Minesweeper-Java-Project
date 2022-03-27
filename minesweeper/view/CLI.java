c6b4bd4c9625b9f1e1a90944d49efd702685d623
cms498 <cms8219@rit.edu>
2022-03-27 15:15:34 -0400

    java docs added

--- minesweeper/view/CLI.java
+++ minesweeper/view/CLI.java
@@ -2,32 +2,44 @@ package minesweeper.view;
 
 import java.util.HashSet;
 import java.util.Scanner;
-
 import minesweeper.model.GameState;
 import minesweeper.model.Location;
 import minesweeper.model.Minesweeper;
 
+/**
+ * This is the basic CLI for the minesweeper game, it has all of the commands
+ * necessary to play, reset, and give the player hints
+ */
 public class CLI {
+    /**
+     * basic game loop
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         final int ROW = 4;
         final int COL = 4;
         final int MINES = 2;
-        Minesweeper game = new Minesweeper(ROW, COL, MINES); //any values
+        Minesweeper game = new Minesweeper(ROW, COL, MINES);
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Mines: " + MINES);
         System.out.println("Commands:");
         System.out.println("\t help - this help message");
         System.out.println("\t pick <row> <col> - uncovers cell a <row> <col>");
         System.out.println("\t hint - displays a safe selection");
         System.out.println("\t reset - resets to a new game");
         System.out.println("\t quit - quits the game");
+        System.out.println("\n" + game);
 
-        while (game.getGameState() != GameState.WON && game.getGameState() != GameState.LOST){
+        while (game.getGameState() != GameState.WON && game.getGameState() != GameState.LOST){ // loops until the game state is won or lost
+            System.out.println("\nMoves: " + game.getMovesCount());
+            System.out.print("\nEnter a command: ");
             String input = scanner.nextLine();
             input = input.toLowerCase();
             String[] tokens = input.split(" ");
 
-            if (tokens[0].equals("help")){
+            if (tokens[0].equals("help")){ // prints out the possible commands 
                 System.out.println("Commands:");
                 System.out.println("\t help - this help message");
                 System.out.println("\t pick <row> <col> - uncovers cell a <row> <col>");
@@ -35,7 +47,7 @@ public class CLI {
                 System.out.println("\t reset - resets to a new game");
                 System.out.println("\t quit - quits the game");
                 System.out.println(game);
-            } else if(tokens[0].equals("pick" )){
+            } else if(tokens[0].equals("pick" )){ // makes a choice on the game board
                 Location newMove = new Location(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
                 game.makeSelection(newMove);
                 if (game.getGameState() == GameState.LOST){
@@ -43,24 +55,25 @@ public class CLI {
                     System.out.println(game.showBoard());
                 }else if(game.getGameState() == GameState.WON){
                     System.out.println("Congratulations");
+                    System.out.println(game.showBoard());
                 } else{
                     System.out.println(game);
                 }
-            } else if (tokens[0].equals("quit")){
+            } else if (tokens[0].equals("quit")){ // quits out of the game
                 break;
-            } else if (tokens[0].equals("reset")){
+            } else if (tokens[0].equals("reset")){ // makes a new game board of the same size and number of mines
                 System.out.println("Resetting the game");
                 Minesweeper newGame  = new Minesweeper(ROW, COL, MINES);
                 game = newGame;
                 System.out.println(game);
-            } else if(tokens[0].equals("hint")){
-                HashSet<Location> moves = new HashSet<>();
+            } else if(tokens[0].equals("hint")){ // gives the user a possible option left on the board
+                HashSet<Location> moves = game.getPossibleSelections();
                 Location[] arrayMoves = moves.toArray(new Location[moves.size()]);
                 System.out.println("Give " + arrayMoves[0] + " a try!");
                 System.out.println(game);
             }
 
             }
-            scanner.close();
+            scanner.close(); // closes the scanner at the end
         }
     }
