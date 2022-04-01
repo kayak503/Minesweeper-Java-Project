2f16a7e22af8ea8fd9a3e5436e7654bc840cf1ed
Nathan2846 <nathanabdelmalek@gmail.com>
2022-04-01 09:23:02 -0400

    GUI in progress

--- minesweeper/view/GUI.java
+++ minesweeper/view/GUI.java
@@ -1,22 +1,35 @@
 package minesweeper.view;
 
+import java.awt.Color;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import minesweeper.model.Minesweeper;
 
 public class GUI extends Application{
+    private static final int ROWS = 4;
+    private static final int COLS = 4;
+    private static final int MINES = 2;
+
     static final Image MINE  = new Image("file:media/images/mine24.png");
 
     private Minesweeper minesweeper;
 
     public GUI(){
-        this.minesweeper = new Minesweeper(4, 4, 2);
+        this.minesweeper = new Minesweeper(ROWS, COLS, MINES);
     }
 
     @Override
@@ -42,6 +55,14 @@ public class GUI extends Application{
         stage.setScene(scene);
         stage.show();
         
+    }
+
+    public static Button makeCoveredButton(){
+        Button button = new Button("");
+        button.setMaxSize(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+        button.setBackground(new Background(new BackgroundFill(Color.RED, new CornerRadii(10), Insets.EMPTY);
+        button.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, new CornerRadii(10), BorderStroke.MEDIUM)));
+        return button;
     }
 
     public static void main(String[] args) {
