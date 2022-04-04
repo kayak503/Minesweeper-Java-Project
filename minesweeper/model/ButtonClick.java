2fd61b53463b3fdcdf7e7c4d3408bc0fbe06415e
Ari Becker <aribecker503@gmail.com>
2022-04-04 16:45:20 -0400

    Button click check if button is disabled

--- minesweeper/model/ButtonClick.java
+++ minesweeper/model/ButtonClick.java
@@ -2,20 +2,25 @@ package minesweeper.model;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 
 public class ButtonClick implements EventHandler<ActionEvent> {
 
 
     private final Minesweeper minesweeper;
     private final Location location;
+    private final Button button;
 
-    public ButtonClick(Minesweeper minesweeper, Location location) {
+    public ButtonClick(Minesweeper minesweeper, Location location, Button button) {
         this.minesweeper = minesweeper;
         this.location = location;
+        this.button = button;
     }
 
     @Override
     public void handle(ActionEvent clickAction) {
+        if (button.isDisabled()){return;}
+
         try {
             minesweeper.makeSelection(location);
         } catch (Exception e) {
