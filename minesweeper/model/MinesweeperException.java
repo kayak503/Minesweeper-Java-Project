7b6a519fda3bc9567fabd31a5e8ec3aa0f5bd4b4
cms498 <cms8219@rit.edu>
2022-04-04 22:15:38 -0400

    comments added

--- minesweeper/model/MinesweeperException.java
+++ minesweeper/model/MinesweeperException.java
@@ -1,8 +1,11 @@
+/**
+ * This program houses the custom exception that the game will throw
+ */
+
 package minesweeper.model;
 
 public class MinesweeperException extends java.lang.Exception{
     public MinesweeperException(String message) throws Exception{
         throw new java.lang.Exception(message);
-    }
-    
+    }   
 }
