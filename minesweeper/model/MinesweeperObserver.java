9f561a2887eca60eb39128a820c59d35732abad0
cms498 <cms8219@rit.edu>
2022-04-04 22:18:59 -0400

    comments added

--- minesweeper/model/MinesweeperObserver.java
+++ minesweeper/model/MinesweeperObserver.java
@@ -1,6 +1,10 @@
+/**
+ * This interface houses the Minesweeper observer which houses two methods
+ */
+
 package minesweeper.model;
 
 public interface MinesweeperObserver {
-    public abstract void cellUpdated(Location location);
-    public abstract void reDrawBoard(Minesweeper minesweeper);
+    public abstract void cellUpdated(Location location); // updates the state of the cells on the board
+    public abstract void reDrawBoard(Minesweeper minesweeper); // redraws the board after the game is over, reset button
 }
