ed28d786c2463a1d639cba6d8334015ae016822e
Ari Becker <aribecker503@gmail.com>
2022-04-06 08:59:36 -0400

    getSuccessors() done!

--- minesweeper/model/MinesweeperSolver.java
+++ minesweeper/model/MinesweeperSolver.java
@@ -9,15 +9,54 @@ import backtracker.Configuration;
 public class MinesweeperSolver implements Configuration{
     private Minesweeper minesweeper;
     private List<Location> locationSolution;
+    private final int ROWS;
+    private final int COLS;
 
-    public MinesweeperSolver(Minesweeper minesweeper){
+    public MinesweeperSolver(Minesweeper minesweeper,int ROWS,int COLS){
         this.minesweeper = minesweeper;
         this.locationSolution = new ArrayList<>();
+        this.ROWS = ROWS;
+        this.COLS = COLS;
     }
+    public MinesweeperSolver(Minesweeper minesweeper,MinesweeperSolver old){
+        this.minesweeper = minesweeper;
+        this.locationSolution = old.locationSolution;
+        this.ROWS = old.ROWS;
+        this.COLS = old.COLS;
+    }
+
 
     @Override
     public Collection<Configuration> getSuccessors() {
         List<Configuration> successors = new ArrayList<>();
+
+        Location nextLocation;
+        
+        if (locationSolution.size() == 0){
+            nextLocation = new Location(0,0);
+        }
+        else{
+            nextLocation = getNextLocation( 
+                locationSolution.get(locationSolution.size()-1) 
+                );
+        }
+        while (successors.size() != 8){
+
+            Minesweeper nextMinesweeper = new Minesweeper(minesweeper);
+
+            try {
+                nextMinesweeper.makeSelection(nextLocation);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            successors.add(
+                new MinesweeperSolver(nextMinesweeper,this)
+            );
+
+            nextLocation = getNextLocation(nextLocation);
+        }
         
         return successors;
     }
@@ -39,5 +78,13 @@ public class MinesweeperSolver implements Configuration{
     @Override
     public String toString(){
         return "";
+    }
+
+
+    private Location getNextLocation(Location location){
+        if (location.getCol() == COLS){
+            return new Location( (location.getRow() +1) % COLS ,0);
+        }
+        return new Location( (location.getRow()), location.getCol()+1 );
     }
 }
